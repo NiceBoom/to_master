ef94518da83436fe1c105bfe32fdb411474ddf7b
Administrator <niceboomgg@gmail.com>
2022-04-20 19:45:24 +0800

    简化冗余代码

--- src/main.go
+++ src/main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 func main() {
@@ -28,7 +29,7 @@ func main() {
 	fmt.Println("============= send weather msg to dingTalk robot of already =====================")
 }
 
-//从高德获取信息
+//从高德获取所有天气数据
 func getWeatherForAmap(weatherUrl string, weatherToken string, cityID string) (string, error) {
 	params := url.Values{}
 	Url, err := url.Parse(weatherUrl)
@@ -46,38 +47,15 @@ func getWeatherForAmap(weatherUrl string, weatherToken string, cityID string) (s
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-
 		}
 	}(resp.Body)
 	body, err := ioutil.ReadAll(resp.Body)
 	var weatherMsg = string(body)
 	fmt.Println(weatherMsg)
-
-	//fmt.Println("============= start weather msg process =====================")
-	//afterMsg := processWeatherMsgForAmap(weatherMsg)
-	//fmt.Println(afterMsg)
-	//fmt.Println("============= process weather msg already =====================")
 	return weatherMsg, nil
 }
 
-//处理消息
-//func processWeatherMsgForAmap(msgAfter string) string {
-//	weatherMsgForecastsBytes := gjson.Get(msgAfter, "forecasts")
-//	s := weatherMsgForecastsBytes.String()
-//	//处理获取到的json，去除外层[]
-//	s1 := s[1 : len(s)-1]
-//	weatherMsgCastsJson := gjson.Get(s1, "casts")
-//	//fmt.Println(weatherMsgCastsJson)
-//	//fmt.Println("==================================")
-//	//weatherMsgCastsString.ForEach(func(key, value gjson.Result) bool {
-//	//	println(value.String())
-//	//	return true
-//	//})
-//	//fmt.Println("================================")
-//	return weatherMsgCastsJson.String()
-//}
-
-//发送钉钉消息
+//向钉钉机器人发送当天天气消息
 func sendWeatherMsgToDingTalkRobot(msgUrl string, msg string) (string, error) {
 	client := &http.Client{}
 	//拼接json参数
@@ -87,7 +65,7 @@ func sendWeatherMsgToDingTalkRobot(msgUrl string, msg string) (string, error) {
 	data2["isAtAll"] = false
 	data1["at"] = data2
 	data3 := make(map[string]interface{})
-	//信息二次处理
+	//处理信息，从所有数据中拿去当天天气消息
 	fmt.Println("===========================start process msg again after sendMsg==================================")
 	afterMsg := dingTalkAfterSendMsgProcess(msg)
 	fmt.Println(afterMsg)
@@ -110,34 +88,9 @@ func sendWeatherMsgToDingTalkRobot(msgUrl string, msg string) (string, error) {
 	return msg, nil
 }
 
-//发送消息前的处理
+//把天气数据处理为当天数据
 func dingTalkAfterSendMsgProcess(msg string) string {
-	//msg1 := msg[1 : len(msg)-1]
-	//var finallyMsg string
-	//msg2.ForEach(func(key, value gjson.Result) bool {
-	//	println(value.String())
-	//	week := gjson.Get(value.String(), "week").String()
-	//	dayWeather := gjson.Get(value.String(), "dayweather").String()
-	//	nightWeather := gjson.Get(value.String(), "nightweather").String()
-	//	highTemp := gjson.Get(value.String(), "daytemp").String()
-	//	lowTemp := gjson.Get(value.String(), "nighttemp").String()
-	//	dayWind := gjson.Get(value.String(), "daywind").String()
-	//	dayPower := gjson.Get(value.String(), "daypower").String()
-	//	nightWind := gjson.Get(value.String(), "nightwind").String()
-	//	nightPower := gjson.Get(value.String(), "nightpower").String()
-	//	finallyMsg = "今天周" + week + "，今天白天" + dayWeather + "，" + dayWind + "风" + dayPower + "级，" +
-	//		"，今天夜间" + nightWeather + "，" + nightWind + "风" + nightPower + "级，" +
-	//		"最高气温:" + highTemp + "度，最低气温：" + lowTemp + "度"
-	//	return true // keep iterating
-	//})
 
-	//fmt.Println(finallyMsg)
-	//获取当天日期
-	//nowDate := time.Now().Format("1992-01-01")
-	//fmt.Println(nowDate)
-	//get1 := gjson.Get(msg, "forecasts.#.casts").String()
-	//fmt.Println(get1)
-	//get := get1[1 : len(get1)-1]
 	week := gjson.Get(msg, `forecasts.#.casts.0.week`)
 	dayWeather := gjson.Get(msg, `forecasts.#.casts.0.dayweather`)
 	nightWeather := gjson.Get(msg, `forecasts.#.casts.0.nightweather`)
@@ -147,10 +100,11 @@ func dingTalkAfterSendMsgProcess(msg string) string {
 	dayPower := gjson.Get(msg, `forecasts.#.casts.0.daypower`)
 	nightWind := gjson.Get(msg, `forecasts.#.casts.0.nightwind`)
 	nightPower := gjson.Get(msg, `forecasts.#.casts.0.nightpower`)
-
-	finallyMsg := "今天周" + week.String()[2:len(week.String())-2] + "，今天白天" + dayWeather.String()[2:len(dayWeather.String())-2] + "，" +
+	today := time.Now().Format("2006-01-02")
+	finallyMsg := "今天是" + today + "星期" + week.String()[2:len(week.String())-2] + "，今天白天" + dayWeather.String()[2:len(dayWeather.String())-2] + "，" +
 		dayWind.String()[2:len(dayWind.String())-2] + "风" + dayPower.String()[2:len(dayPower.String())-2] + "级，" +
-		"今天夜间" + nightWeather.String()[2:len(nightWeather.String())-2] + "，" + nightWind.String()[2:len(nightWind.String())-2] + "风" + nightPower.String()[2:len(nightPower.String())-2] + "级，" +
+		"今天夜间" + nightWeather.String()[2:len(nightWeather.String())-2] + "，" + nightWind.String()[2:len(nightWind.String())-2] + "风" +
+		nightPower.String()[2:len(nightPower.String())-2] + "级，" +
 		"最高气温：" + highTemp.String()[2:len(highTemp.String())-2] + "度，最低气温：" + lowTemp.String()[2:len(lowTemp.String())-2] + "度"
 	return finallyMsg
 }
